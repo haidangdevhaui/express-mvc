262e10c1ccdff40a95d03a569cb0879984fef7c6
vu hai dang <haidangdevhaui@gmail.com>
2018-02-22 14:26:16 +0700

    adding middleware

--- routes/web.js
+++ routes/web.js
@@ -1,6 +1,7 @@
 import * as home_controller from '../app/controllers/home_controller'
+import time_logging from '../app/middlewares/time_logging'
 
 module.exports = (route) => {
 	// defined routes
-	route.get('/', home_controller.index)
+	route.get('/', time_logging, home_controller.index)
 }
