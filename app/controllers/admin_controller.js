b380131d8064494d62ae55ea9a9ddf10637a2948
vu hai dang <haidangdevhaui@gmail.com>
2018-02-27 11:11:43 +0700

    add admin router

---
+++ app/controllers/admin_controller.js
@@ -0,0 +1,3 @@
+export function index(request, response) {
+	response.send('admin');
+}
--- routes/web.js
+++ routes/web.js
@@ -1,7 +1,19 @@
+import { Router } from 'express'
 import * as home_controller from '../app/controllers/home_controller'
+import * as admin_controller from '../app/controllers/admin_controller'
 import time_logging from '../app/middlewares/time_logging'
+import admin_authentication from '../app/middlewares/admin_authentication'
 
 export default function(route) {
 	// defined routes
 	route.get('/', time_logging, home_controller.index)
+
+	// for using prefix and middleware router
+	// const adminRoutes = Router();
+	// route.use('/admin', admin_authentication, adminRoutes)
+	// adminRoutes.get('/', admin_controller.index)
+
+	// for not prefix
+	route.use('/admin*', admin_authentication);
+	route.get('/admin', admin_controller.index);
 }
