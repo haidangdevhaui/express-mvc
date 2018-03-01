59cae38ccf7b24d28a2448c4bcccc95c2bee5ff0
vu hai dang <haidangdevhaui@gmail.com>
2018-03-01 11:44:06 +0700

    category index

--- app/controllers/admin/category_controller.js
+++ app/controllers/admin/category_controller.js
@@ -1,27 +1,49 @@
+import Category from '../../models/category'
+// var Category = require('../../models/category').default;
 
-var Category = require('../../models/category').default;
+/**
+ * category index action - async/await style
+ * @param  {object} request
+ * @param  {object} response
+ */
+export async function index(request, response) {
+    var data = await Category.find();
+    response.render('admin/category/index', {
+        title: 'List category',
+        data: data
+    });
+}
 
-export function index(request, response){
-	var data = Category.find();
-	
-	response.render('admin/category/index',{title: 'List category', data: data});
+/**
+ * category index action - promise/callback style
+ * @param  {object} request
+ * @param  {object} response
+ */
+/*export function index(request, response) {
+	Category.find({}, function(err, categories) {
+		response.render('admin/category/index', {
+	        title: 'List category',
+	        data: categories
+	    });
+	})
+}*/
+
+
+export function create(request, response) {
+    response.render('admin/category/create');
 }
-export function create(request, response){
-	response.render('admin/category/create');
+export function postCreate(request, response) {
+    const newCategory = new Category({
+        name: request.body.name
+    });
+    newCategory.save();
+    // request.flash('info', 'Flash Message Added')
+    response.redirect('/admin/category');
+    // Category.create(request.body.name);
+    // response.send('method post category');
 }
-export function postCreate(request, response){
-	
-	const newCategory = new Category({
-		name: request.body.name
-	});
-	newCategory.save();
-	// request.flash('info', 'Flash Message Added')
-	response.redirect('/admin/category');
-	// Category.create(request.body.name);
-	// response.send('method post category');
-}
-export function edit(request, response){
-	response.render('admin/category/edit');
+export function edit(request, response) {
+    response.render('admin/category/edit');
 }
 // export function postEdit(request, response){
 // 	response.send('method post edit product');
