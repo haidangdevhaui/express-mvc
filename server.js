5e4a16697c5405f59f67c78848c0bbd9a4efbf8b
hoanghongchuong <chuonghoanghong@gmail.com>
2018-03-09 11:31:39 +0700

    deploy

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 import app from './bootstrap/app'
 // running server
-app.listen(process.env.PORT || process.env.APP_PORT, () => console.log(`Server is running on port ${process.env.APP_PORT}`));
+app.listen(process.env.PORT || 3000, () => console.log(`Server is running on port ${process.env.APP_PORT}`));
